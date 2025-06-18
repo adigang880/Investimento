99a7c8af66c7bc7d0b6108a51e86f9390a5264ad
Arthur Ribeiro <102561489+adigang880@users.noreply.github.com>
2025-06-17 22:05:21 -0300

    Fix dataframe handling

--- fundamentalist.py
+++ fundamentalist.py
@@ -183,4 +183,5 @@ def fundamentalist_filters(data, use_defaults):
     df_acao = process_data(data)
     df2 = filter_dataframe(df_acao, use_defaults)
 
-    return df2
+    # Garantir que o retorno seja sempre um DataFrame simples
+    return df2.reset_index(drop=True)
